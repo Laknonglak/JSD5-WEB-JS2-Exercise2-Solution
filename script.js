fa3ed0614da3b965143f32c1774c2e31dbbbd4e3
Laknonglak <donovanlak@gmail.com>
2023-08-16 16:12:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let firstName = "Jack";
+let lastName = "Jones";
+let place = "Spain";
+let hobby = "swimming"
+console.log (`My name is ${firstName} ${lastName}. I live in ${place} and I love ${hobby}`);
